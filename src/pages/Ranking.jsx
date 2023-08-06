df7850dcc553a19a56e77ea8844ee54cff873390
Fernando_Sacerdote <fernando.saerdote@gmail.com>
2023-08-05 22:53:09 -0300

    feat: ranking page layout

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,3 +1,18 @@
+import { styled } from "styled-components"
+import HeaderLogado from "../components/HeaderLogado"
+import ScoreBoard from "../components/ScoreBoard"
+
 export default function Ranking (){
-    
+    return(
+        <RankingPage>
+            <HeaderLogado></HeaderLogado>
+            <ScoreBoard></ScoreBoard>
+        </RankingPage>
+    )
 }
+
+const RankingPage = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
